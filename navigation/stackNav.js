f2dc366432eb85bad0dcb683b4edaae441882c2b
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2022-05-11 09:59:23 -0500

    Update stackNav.js

--- navigation/stackNav.js
+++ navigation/stackNav.js
@@ -11,8 +11,8 @@ export default function StackNav() {
         headerShown: false,
       })}
     >
-      <AppStackNavigation.Screen name="Home" component={HomeScreen} />
-      <AppStackNavigation.Screen name="Movies" component={TopTabNav} />
+      <AppStackNavigation.Screen name="Inicio" component={HomeScreen} />
+      <AppStackNavigation.Screen name="Películas" component={TopTabNav} />
     </AppStackNavigation.Navigator>
   );
 }
